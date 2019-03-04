31e9cf96afd00b1150c683faa768b039ea42c34e
stuart <none>
2019-03-03 19:45:34 -0600

    started on keyboard firmware

--- firmware_v1/firmware_v1.ino
+++ firmware_v1/firmware_v1.ino
@@ -9,9 +9,44 @@ const int NUM_ROWS = 4;
 const int NUM_COLS = 12;
 const int row_pins[NUM_ROWS] = {0, 1, 2, 3};
 const int col_pins[NUM_COLS] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16};
+const int NOP = -1;
+
+// key[0][0] should always be NOP. Logic ignores any defined functions because this is the layer switch key.
+
+/* example layer
+  const int layer[NUM_COLS][NUM_ROWS] = {
+  {NOP,NOP,NOP,NOP,NOP,NOP,  NOP,NOP,NOP,NOP,NOP,NOP},
+  {NOP,NOP,NOP,NOP,NOP,NOP,  NOP,NOP,NOP,NOP,NOP,NOP},
+  {NOP,NOP,NOP,NOP,NOP,NOP,  NOP,NOP,NOP,NOP,NOP,NOP},
+  {NOP,NOP,NOP,NOP,NOP,NOP,  NOP,NOP,NOP,NOP,NOP,NOP}
+  };
+*/
+
+const int layer0[NUM_COLS][NUM_ROWS = {
+  {NOP,     KEY_1, KEY_2, KEY_3, KEY_4, KEY_5,      KEY_6, KEY_7, KEY_8, KEY_9, KEY_0, KEY_BACKSPACE},
+  {KEY_TAB, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T,      KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P, KEY_ENTER},
+  {NOP,     KEY_A, KEY_S, KEY_D, KEY_F, KEY_G,      KEY_H, KEY_J, KEY_K, KEY_L, KEY_UP, KEY_PERIOD},
+  {NOP,     KEY_Z, KEY_X, KEY_C, KEY_V, KEY_SPACE,  KEY_B, KEY_N, KEY_M, KEY_LEFT, KEY_DOWN, KEY_RIGHT}
+};
+
+int layer = 0;
+
+bool new_state[NUM_ROWS][NUM_COLS] = {
+  {false, false, false, false, false, false,   false, false, false, false, false, false},
+  {false, false, false, false, false, false,   false, false, false, false, false, false},
+  {false, false, false, false, false, false,   false, false, false, false, false, false},
+  {false, false, false, false, false, false,   false, false, false, false, false, false}
+};
+
+bool old_state[NUM_ROWS][NUM_COLS] = {
+  {false, false, false, false, false, false,   false, false, false, false, false, false},
+  {false, false, false, false, false, false,   false, false, false, false, false, false},
+  {false, false, false, false, false, false,   false, false, false, false, false, false},
+  {false, false, false, false, false, false,   false, false, false, false, false, false}
+}
 
 int lev_dist(String s1, String s2) {
-  
+
 }
 
 void setup_matrix() {
@@ -30,41 +65,60 @@ void setup() {
   oled.begin();
 }
 
-String read_matrix() {
-  bool noActive = true;
-  String s;
-
+void read_matrix(void) {
   for (int row = 0; row < NUM_ROWS; ++row) {
     for (int col = 0; col < NUM_COLS; ++col) {
       digitalWrite(col_pins[col], LOW);
-      if (!digitalRead(row_pins[row])) {
-        if (!noActive)
-          s += ", ";
-        s += "(" + String(row + 1) + ", " + String(col + 1) + ")";
-        noActive = false;
-      }
+      new_state[row][col] = digitalRead(row_pins[row]);
       digitalWrite(col_pins[col], HIGH);
     }
   }
+}
 
-  return s;
+void update_state(void) {
+  for (int row = 0; row < NUM_ROWS; ++row) {
+    for (int col = 0; col < NUM_COLS; ++col) {
+      old_state[row][col] = new_state[row][col];
+    }
+  }
+
+  read_matrix();
+}
+
+
+
+void keyboard_logic() {
+  for (int row = 0; row < NUM_ROWS; ++row) {
+    for (int col = 0; col < NUM_COLS; ++col) {
+     
+    }
+  }
 }
 
 void loop() {
   oled.clearDisplay();
 
+  update_state();
+
+  keyboard_logic();
+
   oled.setTextSize(1);
   oled.setTextColor(WHITE);
   oled.setCursor(0, 0);
 
-  String s = read_matrix();
+  switch (layer) {
+    case -1:
+      oled.println("layer change layer");
+      break;
+    case 0:
+      oled.println("normal layer");
+      break;
+    default:
+      oled.println("unknown layer");
+  }
 
-  if (s.length() == 0)
-    oled.print("no active keys");
-  else
-    oled.print(s);
 
   oled.display();
 
-  delay(50);//Might be unnecessary
+  delay(10);//Might be unnecessary
 }
