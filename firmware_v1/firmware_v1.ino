693dabc2afafc96965d74f14657d36e740126afa
stuart <none>
2019-03-05 12:06:50 -0600

    added layers and most of the keys expected on a keyboard

--- firmware_v1/firmware_v1.ino
+++ firmware_v1/firmware_v1.ino
@@ -10,28 +10,50 @@ const int NUM_COLS = 12;
 const int row_pins[NUM_ROWS] = {0, 1, 2, 3};
 const int col_pins[NUM_COLS] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16};
 
-// key[0][0] should always be NOP. Logic ignores any defined functions because this is the layer switch key.
+// key[0][0] should always be 0. Logic ignores any defined functions because this is the mode switch key.
+/* modes will be added later. Modes will each have their own layers and logic so for example,
+  the default layer for a WoW mode could have the actionbar keys moved down from the number row for reducing strain while another layer is for typing chat messages.
+  Custom logic should allow modes to change layers in whatever way would be most useful, like a chat layer toggling on and off rather than having to hold a key down.
+*/
 
 /* example layer
   const int layer[NUM_COLS][NUM_ROWS] = {
-  {NOP,NOP,NOP,NOP,NOP,NOP,  NOP,NOP,NOP,NOP,NOP,NOP},
-  {NOP,NOP,NOP,NOP,NOP,NOP,  NOP,NOP,NOP,NOP,NOP,NOP},
-  {NOP,NOP,NOP,NOP,NOP,NOP,  NOP,NOP,NOP,NOP,NOP,NOP},
-  {NOP,NOP,NOP,NOP,NOP,NOP,  NOP,NOP,NOP,NOP,NOP,NOP}
+  {0,0,0,0,0,0,  0,0,0,0,0,0},
+  {0,0,0,0,0,0,  0,0,0,0,0,0},
+  {0,0,0,0,0,0,  0,0,0,0,0,0},
+  {0,0,0,0,0,0,  0,0,0,0,0,0},
   };
 */
 
 const uint8_t layers[][NUM_ROWS][NUM_COLS] =
 {
   {
-    {0,       KEY_1, KEY_2, KEY_3, KEY_4, KEY_5,          KEY_6, KEY_7, KEY_8, KEY_9,     KEY_0, KEY_BACKSPACE},
-    {KEY_TAB, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T,          KEY_Y, KEY_U, KEY_I, KEY_O,     KEY_P, KEY_ENTER},
-    {0,       KEY_A, KEY_S, KEY_D, KEY_F, KEY_G,          KEY_H, KEY_J, KEY_K, KEY_L,     KEY_UP, KEY_PERIOD},
-    {0,       KEY_Z, KEY_X, KEY_C, KEY_V, KEY_SPACE,      KEY_B, KEY_N, KEY_M, KEY_LEFT,  KEY_DOWN, KEY_RIGHT}
+    {0, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5,          KEY_6, KEY_7, KEY_8, KEY_9,     KEY_0, KEY_BACKSPACE},
+    {0, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T,          KEY_Y, KEY_U, KEY_I, KEY_O,     KEY_P, 0},
+    {0, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G,          KEY_H, KEY_J, KEY_K, KEY_L,     KEY_UP, 0},
+    {0, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_SPACE,      KEY_B, KEY_N, KEY_M, KEY_LEFT,  KEY_DOWN, KEY_RIGHT}
+  },
+  {
+    {0, KEY_PERIOD, 0, 0, 0, 0,               0, 0, KEY_EQUAL, KEY_LEFT_BRACE, KEY_RIGHT_BRACE, KEY_ENTER},
+    {0, KEY_QUOTE, 0, KEY_ESC, 0, KEY_TAB,    0, 0, 0, 0, KEY_PRINTSCREEN, 0},
+    {0, 0, 0, 0, 0, 0,                        0, 0, 0, 0, KEY_PAGE_UP, 0},
+    {0, 0, 0, 0, 0, KEY_COMMA,                0, 0, 0, KEY_HOME, KEY_PAGE_DOWN, KEY_END}
+  },
+  {
+    {0, KEY_SLASH, 0, 0, 0, 0, 0,    0, KEY_BACKSLASH, 0, 0, KEY_DELETE},
+    {0, 0, 0, 0, 0, 0,               0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0,               0, 0, 0, 0, KEY_MEDIA_PLAY_PAUSE, 0},
+    {0, 0, 0, 0, 0, KEY_MINUS,       0, 0, 0, KEY_MEDIA_VOLUME_DEC, KEY_MEDIA_MUTE, KEY_MEDIA_VOLUME_INC},
+  },
+  {
+    {0, KEY_SEMICOLON, 0, 0, 0, 0,    0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0,                0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0,                0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0,                0, 0, 0, 0, 0, 0},
   }
 };
 
-int layer = 0;
+unsigned layer = 0;
 
 bool new_state[NUM_ROWS][NUM_COLS] = {
   {false, false, false, false, false, false,   false, false, false, false, false, false},
@@ -120,7 +142,8 @@ void remove_keys() {
   for (int row = 0; row < NUM_ROWS; ++row) {
     for (int col = 0; col < NUM_COLS; ++col) {
       if (!new_state[row][col])
-        remove_key(layers[layer][row][col]);
+        for (int i = 0; i < 4; ++i) //clear everything so keys aren't stuck on from layer switch.
+          remove_key(layers[i][row][col]);
     }
   }
 }
@@ -136,16 +159,31 @@ void add_keys() {
 
 void modifiers() {
   int mods = 0;
-  if (layer == 0) {
-    if (new_state[2][0])
-      mods = mods | MODIFIERKEY_SHIFT;
-    if (new_state[3][0])
-      mods = mods | MODIFIERKEY_CTRL;
-  }
+
+  if (new_state[1][0])
+    mods = mods | MODIFIERKEY_ALT;
+  if (new_state[2][0])
+    mods = mods | MODIFIERKEY_SHIFT;
+  if (new_state[3][0])
+    mods = mods | MODIFIERKEY_CTRL;
+
+  if (new_state[2][5])
+    mods = mods | MODIFIERKEY_GUI;
+
   Keyboard.set_modifier(mods);
 }
 
+void set_layer() {
+  layer = 0;
+  if (new_state[1][11])
+    layer = layer | 1;
+  if (new_state[2][11])
+    layer = layer | 2;
+}
+
 void keyboard_logic() {
+  set_layer();
+
   //Old keys must be removed before new keys are added so there is space in array for new key presses.
   remove_keys();
 
@@ -155,7 +193,6 @@ void keyboard_logic() {
 }
 
 void loop() {
-
   update_state();
 
   keyboard_logic();
@@ -174,20 +211,12 @@ void loop() {
       const int y = col * 5 + (col > 5 ? 5 : 0);
       oled.drawRect(y, x, 5, 5, WHITE);
       if (new_state[row][col])
-        oled.drawPixel(y + 2, x + 2, WHITE);
+        oled.fillRect(y + 1, x + 1, 3, 3, WHITE);
     }
   }
 
-  switch (layer) {
-    case -1:
-      oled.println("layer change layer");
-      break;
-    case 0:
-      oled.println("normal layer");
-      break;
-    default:
-      oled.println("unknown layer");
-  }
+  oled.print("layer ");
+  oled.println(layer);
 
   for (int i = 0; i < 6; ++i) {
     oled.print(keyboard_keys[i]);
